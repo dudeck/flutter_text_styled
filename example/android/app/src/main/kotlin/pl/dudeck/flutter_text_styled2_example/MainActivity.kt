6c31d25c1f28f100c877c6b433fb257c45439ad2
Mateusz Dudkowski <dudek16@gmail.com>
2021-02-19 13:04:22 +0100

    0.3.0 Added color tags feature

---
+++ example/android/app/src/main/kotlin/pl/dudeck/flutter_text_styled2_example/MainActivity.kt
@@ -0,0 +1,6 @@
+package pl.dudeck.flutter_text_styled2_example
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ example/lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_text_styled/flutter_text_styled.dart';
+
+void main() {
+  runApp(StyledPage());
+}
+
+class StyledPage extends StatefulWidget {
+  @override
+  _StyledPageState createState() => _StyledPageState();
+}
+
+class _StyledPageState extends State<StyledPage> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Text Style Demo',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Title"),
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: SingleChildScrollView(
+            child: Wrap(
+              children: TextStyled().getStyledTextWidgets(
+                  "This is normal text [color=blue]blue text[/color] with [b]bold text[/b] [i]italic text[/i] [u]underlined text[/u] and [color=0xFFFFD600][b][i][u]mixed styled[/b][/i][/u][/color]"),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
